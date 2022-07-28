3c30afcea21c91567d9355d4453306caf12ad1a4
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-07-29 00:39:27 +0300

    login separation

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,18 @@ import Table from "./Components/Table";
 //import {MockFaceDetails} from "./Components/MockData/Mock";
 import Pagination from "./Components/Pagination/Pagination";
 import Image from "./Components/Image";
+import './Components/login';
 function App() {
-  AWS.config.region = process.env.REACT_APP_REGION; // Region
-  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: process.env.REACT_APP_POOL_ID as string
-  });
   let [result,setResult]=useState<DetectFacesResponse>({} as DetectFacesResponse);
   const FaceDetails = result?.FaceDetails;
   //const OrientationCorrection = result?.OrientationCorrection;
   let [current,setCurrent] = useState<number>(0);
-  let width:number = 550;
-  let height:number = 550;
-  let image = {height:height,width:width};
+  let image = {height:550,width:550};
   return (
     <div className="App">
       <Image
-          width={width}
-          height={height}
+          width={image.width}
+          height={image.height}
           FaceDetails={FaceDetails}
           image={image}
           setCurrent={setCurrent}
@@ -32,23 +27,19 @@ function App() {
       />
 
       <div className={"container"}>
-
-        {result !== {} as DetectFacesResponse &&
-        <>
-            <Pagination
-                minPage={0}
-                current={current}
-                setCurrent={setCurrent}
-                maxPage={FaceDetails?.length || 0}
-            />
-            <Table
-                id={current}
-                className={"mainTable"} bold={true}
-                key={`${FaceDetails?.at(current)?.AgeRange}~${FaceDetails?.at(current)?.BoundingBox}`}
-                faceDetail={FaceDetails?.at(current)}
-            />
-        </>
-        }
+        <Pagination
+            minPage={0}
+            current={current}
+            setCurrent={setCurrent}
+            maxPage={FaceDetails?.length || 0}
+        />
+        <Table
+            bold={true}
+            id={current}
+            className={"mainTable"}
+            faceDetail={FaceDetails?.at(current)}
+            key={`${FaceDetails?.at(current)?.AgeRange}~${FaceDetails?.at(current)?.BoundingBox}`}
+        />
       </div>
     </div>
   );
