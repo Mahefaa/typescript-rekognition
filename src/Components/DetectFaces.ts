8218692ca0ef8807d5b27728d1da07bb05943c88
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-07-28 22:40:20 +0300

    feat DetectFaces

---
+++ src/Components/DetectFaces.ts
@@ -0,0 +1,21 @@
+import AWS, {AWSError, Rekognition} from "aws-sdk";
+import {DetectFacesResponse} from "aws-sdk/clients/rekognition";
+
+const detectFaces : (imageData: ArrayBuffer,
+                     file: File, callback: (err: AWSError, data: Rekognition.DetectFacesResponse) => void)
+    => void = (imageData,
+               file,
+               callback) =>
+{
+    let rekognition = new AWS.Rekognition();
+    let params = {
+        Image: {
+            Bytes: imageData
+        },
+        Attributes: [
+            'ALL',
+        ]
+    };
+    rekognition.detectFaces(params, callback);
+}
+export default detectFaces;
